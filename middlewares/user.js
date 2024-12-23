96fe3fb0e1a1e7259a8826a74c9f3e857f5d88e9
sourav <mailsourav014@gmail.com>
2024-12-23 18:22:59 +0530

    created admin and user middlewares

--- middlewares/user.js
+++ middlewares/user.js
@@ -1,7 +1,20 @@
+const { User, Admin } = require("../db/index");
 //middleware for handling auth
 function userMiddleware(req, res, next) {
   //implementing user auth
   const username = req.headers.username;
   const password = req.headers.password;
+  User.findOne({
+    username: username,
+    password: password,
+  }).then((value) => {
+    if (value) {
+      next();
+    } else {
+      res.status(403).json({
+        msg: "User does not exist",
+      });
+    }
+  });
 }
 module.exports = userMiddleware;
